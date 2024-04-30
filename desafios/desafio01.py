c59fa88bd2b4488249a6433f9d03d3883a0e54bc
rachelucena <159203793+rachelucena@users.noreply.github.com>
2024-04-30 10:50:59 -0400

    Primeiras aulas de python

---
+++ desafios/desafio01.py
@@ -0,0 +1,3 @@
+print('======= DESAFIO 1 =======')
+nome = input('Qual seu nome? ')
+print('Prazer, ', nome,'! Prazer em te conhecer!')
---
+++ desafios/desafio03.py
@@ -0,0 +1,5 @@
+print('======= DESAFIO 03 =======')
+n1 = input('Primeiro numero: ')
+n2 = input('Segundo numero: ')
+soma = n1 + n2
+print('A soma e ', soma)
