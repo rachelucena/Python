80dbbf748b0725c784770c1d9da741a41d63235e
rachelucena <159203793+rachelucena@users.noreply.github.com>
2024-05-01 15:06:17 -0400

    desafios resolvidos (aula 06)

---
+++ desafios/desafio04.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+soma = n1 + n2
+print('A soma de {0} com {1} e igual a {2}'.format(n1, n2, soma))
---
+++ desafios/desafio05.py
@@ -0,0 +1,6 @@
+n = input('Digite algo: ')
+print(n.isnumeric())
+print(n.isprintable())
+print(n.istitle())
+print(n.isdecimal())
+print(n.isupper())
